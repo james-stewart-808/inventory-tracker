9decfe071790794ef5054b98fa52d9b37294a7e4
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 10:26:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def load_original_data():
         st.error("Failed to load data from GitHub.")
         return None
 country_iso_codes = load_original_data()
+st.write(country_iso_codes)
 
 # Function to load the CSV file
 #@st.cache_data
