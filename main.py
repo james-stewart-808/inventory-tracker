8fb23c7739ffc9f9e8c8e2b41514d1b8d76c8a3a
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 11:04:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ st.header("Welcome to the Homepage of the International Shipping Dashboard 🏡"
 country_iso_codes_c = ["name", "alpha-2", "alpha-3", "country-code"]
 country_iso_codes_d = {"alpha-2":str, "alpha-3":str, "country-code":str}
 country_iso_codes_r = {"name":"iso_country", "alpha-2":"iso_2", "alpha-3":"iso_3", "country-code":"iso_code"}
-#country_iso_codes = pd.read_csv(
-#  "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/country_iso_codes.csv",
-#  usecols=country_iso_codes_cols, 
-#  dtype=country_iso_codes_dtype).rename(
-#  columns=country_iso_codes_renames)
-
 
 country_iso_codes = pd.read_csv(
   "https://raw.githubusercontent.com/james-stewart-808/inventory-tracker/main/datasets/country_iso_codes.csv",
@@ -22,4 +16,15 @@ country_iso_codes = pd.read_csv(
 
 country_iso_codes.loc[country_iso_codes.iso_country == "Namibia", "iso_2"] = "NA"
 st.write(country_iso_codes.head())
-#
+
+country_choice = st.selectbox(
+    "For which country would you like to statistics related to international shipping, merchandise trade and MTM impact tracking?",
+    (country_iso_codes.iso_country.unique()))
+
+# set as global variable
+st.session_state.iso_country = country_choice
+st.session_state.iso_2 = country_iso_codes[(country_iso_codes.iso_country == country_choice)].iso_2.values[0]
+st.session_state.iso_3 = country_iso_codes[(country_iso_codes.iso_country == country_choice)].iso_3.values[0]
+st.session_state.iso_code = country_iso_codes[(country_iso_codes.iso_country == country_choice)].iso_code.values[0]
+
+### Placeholder for map view of country
